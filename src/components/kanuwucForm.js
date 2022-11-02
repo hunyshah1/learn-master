2512a571d31a88c924f2d4ffbbfbf85395bd7de9
Your Name <you@example.com>
2022-11-02 22:29:51 +0500

    done something

--- src/components/kanuwucForm.js
+++ src/components/kanuwucForm.js
@@ -2,24 +2,16 @@ import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete';
 import { useState,useEffect } from 'react';
+import { CircularProgress } from '@mui/material';
 
 
-
+const API = 'http://192.168.10.30:3001/'
 const filterOptions = createFilterOptions({
   matchFrom: 'start',
   stringify: (option) => option.NAME,
 });
-
-export default function KanuUcForm() {
-  // const [error, setError] = useState(undefined);
-  //   const [isLoading, setIsLoaded] = useState(true);
-    const [items, setItems] = useState([]);
-
-
-    
-
-    useEffect(() => {
-      fetch("http://192.168.10.30:3001/kanugo", {
+const fetchData = async(url,callBack)=>{
+  fetch(url, {
         method: "GET",
         headers: { "Content-Type": "application/json" },
       })
@@ -30,8 +22,8 @@ export default function KanuUcForm() {
               (data) => {
                 
     
-                // setIsLoaded(false);
-                setItems(data.data);
+                 
+                callBack(data.data)
                 // console.log(data.data)
               }
                            
@@ -42,18 +34,69 @@ export default function KanuUcForm() {
             //       setError(error);
             //     }
               )
-          }, [])
+}
+export default function KanuUcForm() {
   
-  return (<> <Autocomplete
-      id={items.ID}
+  // const [error, setError] = useState(undefined);
+  //   const [isLoading, setIsLoaded] = useState(true);
+    const [items, setItems] = useState([]);
+    const [ucItems,setUcItems] = useState([]);
+    const [loading,setloading] =useState(true);
+
+    
+
+    useEffect(() => {
+        setloading(true)
+            fetchData(API+'kanugo',(data)=>{
+              setItems(data)
+              setloading(false)
+            })
+          }, [])
+
+      
+          const getComponent = (params)=>{
+            return (
+              !loading?
+              <TextField {...params} label="KanoGo" color="secondary"  />
+              :<h6>Pleas Wait....</h6>
+            )
+          }
+  return (<> 
+  
+ 
+  <Autocomplete
+      onChange={(event,value)=>{
+        if(value){
+        setloading(true)
+       fetchData(API+'uc/'+value.ID,(data)=>{
+        setUcItems(data)
+        setloading(false)
+       
+       })
+      }
+      }}
+      id='k1'
       options={items}
       getOptionLabel={(option) => option.NAME}
       filterOptions={filterOptions}
-      sx={{ width: 200 }}
-      renderInput={(params) => <TextField {...params} label="KanoGo" color="secondary"  />}
+      renderInput={(params) => <TextField {...params} label="KanoGo" color="secondary"  /> }
+
     />
+    {loading?<h6>Wait.....</h6>:
+    <Autocomplete
+    id='c1'
+    options={ucItems}
+    getOptionLabel={(option) => option.UC_NUMBER+""}
+    filterOptions={filterOptions}
+    renderInput={(params) =>  <TextField {...params} label="KanoGo" color="secondary"  />}  
+
+  />
+    
+    }
+
     
     </>
 
   );
+  
 }
